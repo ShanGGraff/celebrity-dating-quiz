bb98bea76fcb9bda181a7288bac69ef52d451aa0
Shane Graff <copellius@gmail.com>
2021-06-16 20:11:13 -0700

    BUGFIX: fix branching logic
    Co-authored-by: Cesar Lopez <lopez.cesar.aug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,24 @@
   
 //UserInterface Logic
 $(document).ready(function () {
-  $(".clickable").click(function () {
-  
+  $("form#dating").submit(function(event) {
+    const hairColor = $("input#hairColor").val().toUpperCase();
+    console.log(hairColor);
+    const footFetish = $("input#footFetish").val().toUpperCase();
+    console.log(footFetish);
+
+    // if (hairColor === "RED" || "GREEN" || "PURPLE" || "BLACK") {
+    //   alert("You're all good");
+    // } else {
+    //   alert("Please Enter Red, Green, Purple or Black");
+    // }
+
+    if (hairColor === "RED" || "GREEN" || "PURPLE" || "BLACK" && footFetish === "YES") {
+      alert("You like Quintin Tarantino");
+    } else {
+      alert("You may like Ron Howard");
+    }
+    // $("#output").text(result);
+    event.preventDefault();
   });
 });
